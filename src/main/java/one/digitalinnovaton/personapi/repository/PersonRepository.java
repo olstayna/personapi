39886b4c97a7f46406316abaa86bd1e9bc3840d3
olstayna <tayna.oliveira.santana@gmail.com>
2021-08-17 16:14:54 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovaton/personapi/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinnovaton.personapi.repository;
+
+import one.digitalinnovaton.personapi.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
